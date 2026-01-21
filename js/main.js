a1b31d2093f101f5830c6cc12cc2b7f9374242e3
FRESH-FARM <7w4w2d7pn9@privaterelay.appleid.com>
2026-01-21 16:41:34 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ window.addEventListener("scroll",()=>{
     header.classList.remove("scrolled");
   }
 });
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", ()=>{
+  reveals.forEach(el=>{
+    const top = el.getBoundingClientRect().top;
+    if(top < window.innerHeight - 100){
+      el.classList.add("active");
+    }
+  });
+});
