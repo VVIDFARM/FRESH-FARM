947edd4f4734ffb1baf88a76903772636aca3db3
FRESH-FARM <7w4w2d7pn9@privaterelay.appleid.com>
2026-01-21 16:19:05 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("FRESH-FARM website active!");
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide() {
+  slides.forEach(s => s.style.display = "none");
+  index++;
+  if (index > slides.length) index = 1;
+  slides[index - 1].style.display = "block";
+}
+
+if (slides.length > 0) {
+  showSlide();
+  setInterval(showSlide, 4000);
+}
