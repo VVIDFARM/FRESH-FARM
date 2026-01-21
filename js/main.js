16a2360c9ba90092948ed483792158a5031f4a86
FRESH-FARM <7w4w2d7pn9@privaterelay.appleid.com>
2026-01-21 16:35:41 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
-let index = 0;
-const slides = document.querySelectorAll(".slide");
-
-function showSlide() {
-  slides.forEach(s => s.style.display = "none");
-  index++;
-  if (index > slides.length) index = 1;
-  slides[index - 1].style.display = "block";
-}
-
-if (slides.length > 0) {
-  showSlide();
-  setInterval(showSlide, 4000);
-}
+console.log("site loaded");
